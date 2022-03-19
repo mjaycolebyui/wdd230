524bc1b51aecc80e6c2c2b758e63317dda365feb
mjaycole <michaeljcoleman95@gmail.com>
2022-03-19 16:35:25 -0600

    spotlights

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,7 +1,7 @@
 const url = 'https://mjaycolebyui.github.io/wdd230/chamber/data/directory.json';
-var spolight1 = document.querySelector('.spotlight-1');
-var spolight2 = document.querySelector('.spotlight-2');
-var spolight3 = document.querySelector('.spotlight-3');
+var spotlight1 = document.querySelector('.spotlight-1');
+var spotlight2 = document.querySelector('.spotlight-2');
+var spotlight3 = document.querySelector('.spotlight-3');
 
 
 getData();
@@ -18,9 +18,9 @@ function getData(){
 }
 
 function fillSpotlights(jsonObject) {
-    spolight1.innerHTML = "";
-    spolight2.innerHTML = "";
-    spolight3.innerHTML = "";
+    spotlight1.innerHTML = "";
+    spotlight2.innerHTML = "";
+    spotlight3.innerHTML = "";
 
     let index = 0;
 
@@ -32,11 +32,48 @@ function fillSpotlights(jsonObject) {
 
     let bizIcon = document.createElement('img');
     bizIcon.src = jsonObject.businesses[index].image;
-    bizIcon.style.height = '100px';
+    bizIcon.style.height = '70px';
 
-    spolight1.append(bizName);
-    spolight1.append(bizIcon);
+    let bizTag = document.createElement('p');
+    bizTag.textContent = jsonObject.businesses[index].tag;
 
+    spotlight1.append(bizName);
+    spotlight1.append(bizIcon);
+    spotlight1.append(bizTag);
+
+    //Create the stuff for spotlight 2
+    index = getRandomInt(0, jsonObject.businesses.length);
+
+    let bizName2 = document.createElement('h3');
+    bizName2.textContent = jsonObject.businesses[index].name;
+
+    let bizIcon2 = document.createElement('img');
+    bizIcon2.src = jsonObject.businesses[index].image;
+    bizIcon2.style.height = '70px';
+
+    let bizTag2 = document.createElement('p');
+    bizTag2.textContent = jsonObject.businesses[index].tag;
+
+    spotlight2.append(bizName2);
+    spotlight2.append(bizIcon2);
+    spotlight2.append(bizTag2);
+
+    //Create the stuff for spotlight 3
+    index = getRandomInt(0, jsonObject.businesses.length);
+
+    let bizName3 = document.createElement('h3');
+    bizName3.textContent = jsonObject.businesses[index].name;
+
+    let bizIcon3 = document.createElement('img');
+    bizIcon3.src = jsonObject.businesses[index].image;
+    bizIcon3.style.height = '70px';
+
+    let bizTag3 = document.createElement('p');
+    bizTag3.textContent = jsonObject.businesses[index].tag;
+
+    spotlight3.append(bizName3);
+    spotlight3.append(bizIcon3);
+    spotlight3.append(bizTag3);
 }
 
 function getRandomInt(min, max) {
